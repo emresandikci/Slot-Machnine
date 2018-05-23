a45c35e21af492bc964d3c7e836bd790900fea2e
Emre Sandıkçı <me@emresandikci.com>
2018-05-23 04:04:00 +0300

    project upload

---
+++ WFASlotMakinasi/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WFASlotMakinasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbCheckPoint1 = new System.Windows.Forms.PictureBox();
+            this.pbCheckPoint2 = new System.Windows.Forms.PictureBox();
+            this.pbCheckPoint3 = new System.Windows.Forms.PictureBox();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblMusteriJeton = new System.Windows.Forms.Label();
+            this.lblKazanmaSansi = new System.Windows.Forms.Label();
+            this.lblKazanmaDurumu = new System.Windows.Forms.Label();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnKapat = new System.Windows.Forms.Button();
+            this.lblKasadakiJeton = new System.Windows.Forms.Label();
+            this.pbKol = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheckPoint1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheckPoint2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheckPoint3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCheckPoint1
+            // 
+            this.pbCheckPoint1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbCheckPoint1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pbCheckPoint1.Location = new System.Drawing.Point(91, 206);
+            this.pbCheckPoint1.Name = "pbCheckPoint1";
+            this.pbCheckPoint1.Size = new System.Drawing.Size(145, 168);
+            this.pbCheckPoint1.TabIndex = 0;
+            this.pbCheckPoint1.TabStop = false;
+            // 
+            // pbCheckPoint2
+            // 
+            this.pbCheckPoint2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbCheckPoint2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pbCheckPoint2.Location = new System.Drawing.Point(264, 206);
+            this.pbCheckPoint2.Name = "pbCheckPoint2";
+            this.pbCheckPoint2.Size = new System.Drawing.Size(150, 168);
+            this.pbCheckPoint2.TabIndex = 0;
+            this.pbCheckPoint2.TabStop = false;
+            // 
+            // pbCheckPoint3
+            // 
+            this.pbCheckPoint3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pbCheckPoint3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pbCheckPoint3.Location = new System.Drawing.Point(440, 206);
+            this.pbCheckPoint3.Name = "pbCheckPoint3";
+            this.pbCheckPoint3.Size = new System.Drawing.Size(144, 168);
+            this.pbCheckPoint3.TabIndex = 0;
+            this.pbCheckPoint3.TabStop = false;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.BackColor = System.Drawing.Color.Silver;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(68, 546);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(532, 62);
+            this.lblDurum.TabIndex = 2;
+            this.lblDurum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMusteriJeton
+            // 
+            this.lblMusteriJeton.BackColor = System.Drawing.Color.Transparent;
+            this.lblMusteriJeton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblMusteriJeton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMusteriJeton.ForeColor = System.Drawing.Color.White;
+            this.lblMusteriJeton.Location = new System.Drawing.Point(539, 411);
+            this.lblMusteriJeton.Name = "lblMusteriJeton";
+            this.lblMusteriJeton.Size = new System.Drawing.Size(46, 41);
+            this.lblMusteriJeton.TabIndex = 3;
+            this.lblMusteriJeton.Text = "0";
+            this.lblMusteriJeton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblMusteriJeton.Click += new System.EventHandler(this.lblMusteriJeton_Click);
+            // 
+            // lblKazanmaSansi
+            // 
+            this.lblKazanmaSansi.AutoSize = true;
+            this.lblKazanmaSansi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKazanmaSansi.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblKazanmaSansi.Location = new System.Drawing.Point(301, 410);
+            this.lblKazanmaSansi.Name = "lblKazanmaSansi";
+            this.lblKazanmaSansi.Size = new System.Drawing.Size(24, 25);
+            this.lblKazanmaSansi.TabIndex = 3;
+            this.lblKazanmaSansi.Text = "0";
+            // 
+            // lblKazanmaDurumu
+            // 
+            this.lblKazanmaDurumu.AutoSize = true;
+            this.lblKazanmaDurumu.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKazanmaDurumu.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblKazanmaDurumu.Location = new System.Drawing.Point(86, 406);
+            this.lblKazanmaDurumu.Name = "lblKazanmaDurumu";
+            this.lblKazanmaDurumu.Size = new System.Drawing.Size(191, 29);
+            this.lblKazanmaDurumu.TabIndex = 3;
+            this.lblKazanmaDurumu.Text = "Kazanma Şansı";
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackgroundImage = global::WFASlotMakinasi.Properties.Resources.minimize;
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMinimize.ForeColor = System.Drawing.Color.White;
+            this.btnMinimize.Location = new System.Drawing.Point(604, 3);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(25, 25);
+            this.btnMinimize.TabIndex = 4;
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnKapat
+            // 
+            this.btnKapat.BackgroundImage = global::WFASlotMakinasi.Properties.Resources.exit;
+            this.btnKapat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnKapat.ForeColor = System.Drawing.Color.White;
+            this.btnKapat.Location = new System.Drawing.Point(635, 3);
+            this.btnKapat.Name = "btnKapat";
+            this.btnKapat.Size = new System.Drawing.Size(25, 25);
+            this.btnKapat.TabIndex = 4;
+            this.btnKapat.UseVisualStyleBackColor = true;
+            this.btnKapat.Click += new System.EventHandler(this.btnKapat_Click);
+            // 
+            // lblKasadakiJeton
+            // 
+            this.lblKasadakiJeton.AutoSize = true;
+            this.lblKasadakiJeton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKasadakiJeton.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblKasadakiJeton.Location = new System.Drawing.Point(394, 406);
+            this.lblKasadakiJeton.Name = "lblKasadakiJeton";
+            this.lblKasadakiJeton.Size = new System.Drawing.Size(0, 13);
+            this.lblKasadakiJeton.TabIndex = 3;
+            this.lblKasadakiJeton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbKol
+            // 
+            this.pbKol.BackColor = System.Drawing.Color.Transparent;
+            this.pbKol.BackgroundImage = global::WFASlotMakinasi.Properties.Resources.handle;
+            this.pbKol.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbKol.Location = new System.Drawing.Point(606, 356);
+            this.pbKol.Name = "pbKol";
+            this.pbKol.Size = new System.Drawing.Size(74, 283);
+            this.pbKol.TabIndex = 5;
+            this.pbKol.TabStop = false;
+            this.pbKol.Click += new System.EventHandler(this.pbKol_Click);
+            this.pbKol.DoubleClick += new System.EventHandler(this.pbKol_DoubleClick);
+            this.pbKol.MouseEnter += new System.EventHandler(this.pbKol_MouseEnter);
+            this.pbKol.MouseLeave += new System.EventHandler(this.pbKol_MouseLeave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(242, 458);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::WFASlotMakinasi.Properties.Resources.bg2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(672, 637);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pbKol);
+            this.Controls.Add(this.btnKapat);
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.lblKazanmaDurumu);
+            this.Controls.Add(this.lblKasadakiJeton);
+            this.Controls.Add(this.lblKazanmaSansi);
+            this.Controls.Add(this.lblMusteriJeton);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.pbCheckPoint3);
+            this.Controls.Add(this.pbCheckPoint2);
+            this.Controls.Add(this.pbCheckPoint1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Tomato;
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheckPoint1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheckPoint2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCheckPoint3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbKol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCheckPoint1;
+        private System.Windows.Forms.PictureBox pbCheckPoint2;
+        private System.Windows.Forms.PictureBox pbCheckPoint3;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblMusteriJeton;
+        private System.Windows.Forms.Label lblKazanmaSansi;
+        private System.Windows.Forms.Label lblKazanmaDurumu;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnKapat;
+        private System.Windows.Forms.Label lblKasadakiJeton;
+        private System.Windows.Forms.PictureBox pbKol;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WFASlotMakinasi/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WFASlotMakinasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Global Tanımlamalar
+
+        Timer Basla = new Timer();
+
+        Random rnd = new Random();
+        SoundPlayer ses = new SoundPlayer();
+        int check1 = 0, check2 = 0, check3 = 0, MusteriJeton = 0, KasadakiJeton = 0, Deger = 0, Deger2 = 0, Deger3 = 0, Kazanma = 0;
+
+        string[] CheckPoints = { "apple", "Orange", "pear", "Banana", "melon", "pepper", "Cherry", "Peach", "pumpkin" };
+
+        #endregion
+
+        #region Buttons
+        private void pbKol_Click(object sender, EventArgs e)
+        {
+            if (MusteriJeton > 0)
+            {
+                ses.SoundLocation = "../../Sounds/add.wav";
+                ses.Play();
+                OyunuBaslat();
+                Basla.Tick += Basla_Tick;
+            }
+            else
+            {
+                lblDurum.Text = "Lütfen Jeton Atınız(Jeton Yazısına Tıkla)";
+            }
+        }
+
+        private void pbKol_DoubleClick(object sender, EventArgs e)
+        {
+            if (MusteriJeton > 0)
+            {
+                KasadakiJeton += MusteriJeton;
+                Basla.Stop();
+                KazanmaOlayi();
+            }
+        }
+
+        private void lblMusteriJeton_Click(object sender, EventArgs e)
+        {
+            ses.SoundLocation = "../../Sounds/jeton.wav";
+            ses.Play(); MusteriJeton++;
+            lblMusteriJeton.Text = MusteriJeton.ToString();
+        } 
+        #endregion
+
+        #region Metodlar
+
+        private void OyunuBaslat()
+        {
+            lblKasadakiJeton.Text = "Kasadaki Jeton :\n" + KasadakiJeton.ToString(); //i = 0;
+            lblDurum.Text = "başladı(Durdurmak İçin Kola Çift Tıkla)";
+            Basla.Start();
+        }
+
+        private void KazanmaOlayi()
+        {
+
+            check1 = Convert.ToInt32(pbCheckPoint1.Tag);
+            check2 = Convert.ToInt32(pbCheckPoint2.Tag);
+            check3 = Convert.ToInt32(pbCheckPoint3.Tag);
+            label2.Text = "" + check1 + "-" + check2 + "-" + check3;
+            if (check1 == check2 && check1 == check3 && check2 == check3)
+            {
+                lblDurum.Text = "KAZANDINIZ"; Kazanma++; lblKasadakiJeton.Text = "Kazandın";
+            }
+            else lblDurum.Text = "KAYBETTİNİZ"; lblKasadakiJeton.Text = "Kasadaki Jeton :\n" + KasadakiJeton.ToString();
+        }
+
+        #region Oyunu Kazanma Şansları
+
+        private void KazanmaSansi05()
+        {
+            lblKazanmaSansi.Text = "%5";
+            Deger = rnd.Next(0, 9); Deger2 = rnd.Next(0, 9); Deger3 = rnd.Next(0, 9);
+        }
+
+        private void KazanmaSansi20()
+        {
+            lblKazanmaSansi.Text = "%25";
+            Deger = rnd.Next(0, 5); Deger2 = rnd.Next(0, 5); Deger3 = rnd.Next(0, 5);
+        }
+
+        #endregion
+
+        #endregion
+
+        void Basla_Tick(object sender, EventArgs e)
+        {
+            #region Kazanma Şansı Olayı
+
+            #region Kazanma Şansı %5
+
+            if (KasadakiJeton < 10)
+            {
+                KazanmaSansi05();
+            }
+            #endregion
+
+            #region Kazanma Şansı %20
+
+            else if (KasadakiJeton > 10)
+            {
+
+                if (Kazanma > 0)
+                {
+                    KazanmaSansi05();
+                }
+                else
+                {
+                    KazanmaSansi20();
+                }
+            }
+            #endregion
+
+            #endregion
+
+            #region Resim Atamaları
+            pbCheckPoint1.ImageLocation = "../../Resources/" + CheckPoints[Deger] + ".png";
+            pbCheckPoint1.BackgroundImageLayout = ImageLayout.Stretch;
+            pbCheckPoint1.Tag = Deger;
+            pbCheckPoint2.ImageLocation = "../../Resources/" + CheckPoints[Deger2] + ".png";
+            pbCheckPoint2.BackgroundImageLayout = ImageLayout.Stretch;
+            pbCheckPoint2.Tag = Deger2;
+            pbCheckPoint3.ImageLocation = "../../Resources/" + CheckPoints[Deger3] + ".png";
+            pbCheckPoint3.Tag = Deger3;
+            pbCheckPoint3.BackgroundImageLayout = ImageLayout.Stretch;
+            //i++;
+            #endregion
+
+            //if (i == 50)
+            //{
+            //    Basla.Stop();
+            //    KazanmaOlayi();
+            //}
+        }
+
+        #region WindowsState
+
+        private void btnKapat_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
+        #region Kol Ayarları
+        private void pbKol_MouseEnter(object sender, EventArgs e)
+        {
+            pbKol.BackgroundImage = Image.FromFile("../../Resources/handle2.png");
+            pbKol.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void pbKol_MouseLeave(object sender, EventArgs e)
+        {
+            pbKol.BackgroundImage = Image.FromFile("../../Resources/handle.png");
+            pbKol.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+        #endregion
+
+        #region EMRE SANDIKÇI
+
+        #endregion
+    }
+}
